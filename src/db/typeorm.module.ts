46dad863737a7c045816cfb2cf8d84cdb7a56929
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2024-10-10 12:39:44 +0300

    setInterval
    clear console.log in typeOrm, add setInterval for wakeup server

--- src/db/typeorm.module.ts
+++ src/db/typeorm.module.ts
@@ -1,12 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule as MySqlTypeOrmModule } from '@nestjs/typeorm';
 
-console.log(`host: ${process.env.MYSQL_HOST} `);
-console.log(`port: ${process.env.MYSQL_PORT} `);
-console.log(`username: ${process.env.MYSQL_USERNAME} `);
-console.log(`password: ${process.env.MYSQL_PASSWORD} `);
-console.log(`database: ${process.env.MYSQL_DATABASE} `);
-
 @Module({
   imports: [
     MySqlTypeOrmModule.forRoot({
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+setInterval(() => {
+  console.log(`datetime: ${new Date()}`);
+}, 300000);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
