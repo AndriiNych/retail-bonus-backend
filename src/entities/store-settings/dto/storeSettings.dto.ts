92cdc9c0e2078fc1d5fc3ce3e0923bcf8f5ca6bb
AndriyNich <163189518+AndriiNych@users.noreply.github.com>
2024-10-28 10:36:38 +0200

    change validate for storeSettings

--- src/entities/store-settings/dto/storeSettings.dto.ts
+++ src/entities/store-settings/dto/storeSettings.dto.ts
@@ -5,9 +5,24 @@ import {
   IsOptional,
   IsDate,
   Matches,
-  ValidateIf,
   MaxLength,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+  Validate,
 } from 'class-validator';
+
+@ValidatorConstraint({ name: 'isLessThanOrEqualTo100', async: false })
+export class IsLessThanOrEqualTo100 implements ValidatorConstraintInterface {
+  validate(value: string, args: ValidationArguments) {
+    const numberValue = parseFloat(value);
+    return numberValue <= 100;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `startBonus must be less than or equal to 100`;
+  }
+}
 
 export class StoreSettingsDto {
   @IsString()
@@ -24,7 +39,7 @@ export class StoreSettingsDto {
 
   @IsOptional()
   @IsString()
-  @ValidateIf(o => parseFloat(o.currentBonus) <= 100)
+  @Validate(IsLessThanOrEqualTo100)
   @Matches(/^\d{1,3}(\.\d{1,2})?$/, {
     message:
       'startBonus must be a decimal number with up to 3 digits before the decimal and 2 digits after',
@@ -33,19 +48,19 @@ export class StoreSettingsDto {
 
   @IsOptional()
   @IsString()
+  @Validate(IsLessThanOrEqualTo100)
   @Matches(/^\d{1,3}(\.\d{1,2})?$/, {
     message:
       'currentBonus must be a decimal number with up to 3 digits before the decimal and 2 digits after',
   })
-  @ValidateIf(o => parseFloat(o.currentBonus) <= 100)
   currentBonus?: string;
 
   @IsOptional()
   @IsString()
+  @Validate(IsLessThanOrEqualTo100)
   @Matches(/^\d{1,3}(\.\d{1,2})?$/, {
     message:
       'bonusPayment must be a decimal number with up to 3 digits before the decimal and 2 digits after',
   })
-  @ValidateIf(o => parseFloat(o.currentBonus) <= 100)
   bonusPayment?: string;
 }
