f0d21323ad1a849c848510f0c4678e6fdbf797f9
AndriyNich <163189518+AndriiNych@users.noreply.github.com>
2024-11-19 16:29:23 +0200

    check receipt.dto

--- src/entities/receipt/dto/receipt.dto.ts
+++ src/entities/receipt/dto/receipt.dto.ts
@@ -5,59 +5,82 @@ import {
   IsDate,
   IsInt,
   IsNotEmpty,
+  IsOptional,
   IsString,
   Matches,
+  Max,
   MaxLength,
   Min,
 } from 'class-validator';
 
 export class ReceiptDto {
+  @IsInt()
+  @IsNotEmpty()
+  @Min(1)
+  @Max(2)
+  type: number;
+
   @IsString()
   @IsNotEmpty()
   @MaxLength(FIELDS_LENGTH.UUID)
   uuid: string;
 
-  @IsString()
-  @MaxLength(FIELDS_LENGTH.NAME)
-  @IsNotEmpty()
-  numberDocument: string;
-
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
-  dateDocument: Date;
+  date: Date;
 
   @IsString()
   @IsNotEmpty()
   @MaxLength(FIELDS_LENGTH.UUID)
+  returnUuid: string;
+
+  @IsString()
+  @Matches(/^\d+(\.\d{1,2})?$/, {
+    message: MSG.ERR.VALIDATION.decimal('totalAmount'),
+  })
+  totalAmount: string;
+
+  @IsString()
+  @Matches(/^\d+(\.\d{1,2})?$/, {
+    message: MSG.ERR.VALIDATION.decimal('accruedBonus'),
+  })
+  accruedBonus: string;
+
+  @IsString()
+  @Matches(/^\d+(\.\d{1,2})?$/, {
+    message: MSG.ERR.VALIDATION.decimal('spentBonus'),
+  })
+  spentBonus: string;
+
+  @IsString()
+  @Matches(/^-?\d+(\.\d{1,2})?$/, {
+    message: MSG.ERR.VALIDATION.decimal('spentBonus'),
+  })
+  saving: string;
+
+  @IsString()
+  @MaxLength(FIELDS_LENGTH.UUID)
+  @IsNotEmpty()
   storeUuid: string;
 
   @IsString()
-  @IsNotEmpty()
   @MaxLength(FIELDS_LENGTH.UUID)
+  @IsNotEmpty()
   workerUuid: string;
 
-  @IsInt()
-  @Min(1)
-  customerId: number;
-
-  @Matches(/^\d+(\.\d{1,2})?$/, {
-    message: MSG.ERR.VALIDATION.decimal('amountBox'),
-  })
   @IsString()
-  amountDocument: string;
+  @IsNotEmpty()
+  @MaxLength(FIELDS_LENGTH.PHONE)
+  customerPhone: string;
 
-  @Matches(/^\d+(\.\d{1,2})?$/, {
-    message: MSG.ERR.VALIDATION.decimal('amountBox'),
-  })
-  @IsString()
-  amountAction: string;
-
+  @IsOptional()
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
   startDate: Date;
 
+  @IsOptional()
   @IsNotEmpty()
   @Type(() => Date)
   @IsDate()
