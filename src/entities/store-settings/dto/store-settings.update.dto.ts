d68f7cfec86d04fb8b341bfaa9c1aa4ca16c78f6
AndriyNich <163189518+AndriiNych@users.noreply.github.com>
2024-12-20 15:24:21 +0200

    change PartialType

--- src/entities/store-settings/dto/store-settings.update.dto.ts
+++ src/entities/store-settings/dto/store-settings.update.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { StoreSettingsDto } from './store-settings.dto';
 
 export class StoreSettingsUpdateDto extends PartialType(StoreSettingsDto) {}
