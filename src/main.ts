2519d73e81db62e16231bd81d7e7d87fa3340efd
AndriyNich <163189518+AndriiNych@users.noreply.github.com>
2024-12-27 16:16:08 +0200

    add CORS

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,12 @@ startActivateOnrenderServer();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
   app.useGlobalInterceptors(
     new ClassSerializerInterceptor(app.get(Reflector), {
       excludeExtraneousValues: true,
